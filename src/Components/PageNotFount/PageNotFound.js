bbf2f3165fccbb2cf08e198c01c776c2b56da94a
Sumeli Debdas <sumeli.debdas22@gmail.com>
2022-02-10 22:06:23 +0530

    first commit

--- src/Components/PageNotFount/PageNotFound.js
+++ src/Components/PageNotFount/PageNotFound.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import './PageNotFound.scss'
 
 const PageNotFound = () => {
     return (
-        <div>
-            PageNotFound
+        <div className='page-not-found'>
+            <h1>
+                Page Not Found 😥. Click on the Logo to go back to Home Page 👆.
+            </h1>
         </div>
     )
 }
