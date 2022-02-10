7b761af28e83f951235f888cd358b0c7950062cf
Sumeli Debdas <72566142+sumeli@users.noreply.github.com>
2022-02-10 23:32:37 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           <Routes>
             <Route path='/' element={<Home />} />
             <Route path='/movie/:imdbID' element={<MovieDetails />} />
-            <Route path='*' element={<PageNotFound />} />
+//             <Route path='*' element={<PageNotFound />} />
           </Routes>
         </div>
         <Footer />
